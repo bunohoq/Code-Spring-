8a8bc9e37ba0001338d8ca642d982cdb155e8339
bunohoq <boyoung5187@naver.com>
2025-10-23 17:28:43 +0900

    MyBatis 연동 및 JUnit 테스트 환경 설정

---
+++ src/main/java/com/test/code/model/CodeDAOImpl.java
@@ -0,0 +1,48 @@
+package com.test.code.model;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class CodeDAOImpl implements CodeDAO {
+	
+	private final SqlSessionTemplate template;
+
+	@Override
+	public List<LanguageDTO> llist() {
+		
+		return template.selectList("code.llist");
+	}
+
+	@Override
+	public int add(CodeDTO dto) {
+		
+		return template.insert("code.add", dto);
+	}
+
+	@Override
+	public List<CodeDTO> list() {
+		
+		return template.selectList("code.list");
+	}
+
+	@Override
+	public CodeDTO get(String seq) {
+		
+		return template.selectOne("code.get", seq);
+	}
+
+}
+
+
+
+
+
+
+
+
